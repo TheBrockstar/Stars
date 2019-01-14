58ca38ed949fe6031803b0e0ec52a2283144c6a2
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-13 23:26:25 -0700

    Built Temporary Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="bmr-container">
+      <div class="bmr-name">Brock Maraee Rohloff</div>
+      <img src="./assets/logo-grey.png">
+      <div class="navigation">
+        <a href="#">Story</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +20,39 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #8ED4DA;
+  padding: 25px 5%;
+}
+
+img {
+  height: 100%;
+}
+
+a {
+  text-decoration: none;
+  color: #424242;
+  font-weight: bold;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
+
+#bmr-container {
+  height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 30px;
+  font-weight: bold;
+  word-spacing: 15px;
+}
+
 </style>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="welcome">
+    <h1>{{ tagline }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      tagline: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
